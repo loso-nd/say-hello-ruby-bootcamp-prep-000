6eeba1eac129cad3600036ac4dd30d6e85c25e8f
Nahomie ND <nducasse3@gmail.com>
2020-12-09 19:42:58 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
-def say_hello(name ="Ruby Programmer")
-  print "Hello #{name}!"
+def say_hello(name ="Ruby Programmer!")
+  print "Hello #{name}"
 end
 
-say_hello("Kent Beck")
+say_hello("Kent Beck!")
 
 
 
